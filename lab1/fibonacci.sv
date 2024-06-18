ab13ede3bad5d7c312501ab7725f5fa4f507c8d6
Baichuan Wei <3362484152@qq.com>
2024-06-18 21:18:16 +0800

    finish fibonacci

--- lab1/fibonacci.sv
+++ lab1/fibonacci.sv
@@ -1,19 +1,19 @@
 module fibonacci (input logic clk, rst_n,
-                  input logic [4:0] input_s, // find the (input_s)'th number in fibonacci list
+                  input logic[4:0] input_s, // find the (input_s)'th number in fibonacci list
                   input logic begin_fibo, // start to calculate the fibonacci
-                 output logic [15:0] fibo_out,
+                 output logic[15:0] fibo_out,
                  output logic done);
-    function automatic void swap(ref logic [15:0] x, ref logic [15:0] y);
-        int mid = x;
+    function automatic void swap(ref logic[15:0] x, ref logic[15:0] y);
+        logic[15:0] mid = x;
         x = y;
         y = mid;
     endfunction
 
-    typedef enum {IDLE, LOAD, RUN} state_t;
-    state_t state, next;
+    typedef enum {IDLE, LOAD, RUN} state_e;
+    state_e state, next;
 
-    logic  [4:0] count;
-    logic [15:0] f0, f1;
+    logic[4:0] count;
+    logic[15:0] f0, f1;
     always_ff @(posedge clk) begin
         if (~ rst_n) begin
             state <= IDLE;
@@ -24,37 +24,25 @@ module fibonacci (input logic clk, rst_n,
         end
     end
 
-    always_comb begin
+    always_ff @(posedge clk) begin
         case (state)
             IDLE:
-                if (begin_fibo) next = LOAD;
-            LOAD:
-                next = RUN;
+                if (begin_fibo) begin
+                    next <= LOAD;
+                    count <= input_s;
+                    f0 <= 0;
+                    f1 <= 1;
+                end
             RUN:
-                if (count) begin
-                    count = count - 1;
-                    f0 = f0 + f1;
-                    swap(f0, f1);
+                if (count != 5'b0) begin
+                    count <= count - 1;
+                    f1 <= f0 + f1;
+                    f0 <= f1;
                 end else begin
-                    next = IDLE;
+                    next <= IDLE;
                 end
             default:
-                next = IDLE;
+                next <= IDLE;
         endcase
-    end
-
-    always_ff @(posedge clk) begin
-        if (~ rst_n) begin
-            count <= 0;
-            f0 <= 0;
-            f1 <= 0;
-        end else if (state == LOAD) begin
-            count <= fibo_out;
-            f0 <= 0;
-            f1 <= 1;
-        end else if (state == RUN && next == IDLE) begin
-            done <= 1;
-            fibo_out <= f1;
-        end
     end
 endmodule
